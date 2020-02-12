1f98d1f0e8cddbb558147a9e4b0030e057c20e5c
jinho6225 <jinho6225@hotmail.com>
2020-02-12 12:06:17 -0800

    router was applied

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import  React from "react";
+
+function About(props) {
+  return (
+    <div className="about__container">
+      <span>About this pages: I build it because i love movies</span>
+    </div>
+  )
+}
+
+export default About;
