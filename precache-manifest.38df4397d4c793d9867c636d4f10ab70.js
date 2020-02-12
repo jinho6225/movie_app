4971ab5cb726d839fb39db68353b4a437a3ed0fe
jinho6225 <jinho6225@hotmail.com>
2020-02-11 20:58:38 -0800

    Updates

---
+++ precache-manifest.38df4397d4c793d9867c636d4f10ab70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee13325a30c6ba0c09f6ca21fcd140a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1b9ec1276f3b65689ab5",
+    "url": "/movie_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "8361fb74a0ab5567e890",
+    "url": "/movie_app/static/js/2.f7b2e7c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.f7b2e7c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9ec1276f3b65689ab5",
+    "url": "/movie_app/static/js/main.b59bd16e.chunk.js"
+  },
+  {
+    "revision": "7dc61f0df4d8741b83c7",
+    "url": "/movie_app/static/js/runtime-main.1dc1365c.js"
+  }
+]);
