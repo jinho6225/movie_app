1441325d92eed53d5250adf622fbada1495bca35
jinho6225 <jinho6225@hotmail.com>
2020-06-14 21:09:51 -0700

    api query updated by rating with limit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from '../components/Movie'
-import "./Home.css"
+import Movie from '../components/Movie';
+import './Home.css';
 
 class Home extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
   getMovies = async () => {
     const {
-      data : {
-        data: { movies }
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&limit=10`
+    );
     this.setState({ movies, isLoading: false });
-  }
+  };
   componentDidMount() {
     this.getMovies();
   }
@@ -23,28 +25,28 @@ class Home extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-    <section className="container">
-      {isLoading ? (
-      <div className="loader">
-      <span className="loader__text">Loading...</span>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
         ) : (
           <div className="movies">
-            {movies.map(movie => (
+            {movies.map((movie) => (
               <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
         )}
-    </section>
-    )
+      </section>
+    );
   }
 }
 
